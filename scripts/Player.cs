dd2c77186e85fefcc114329b23efdf163fb5a48d
DillonJEvans <dillonjayevans@gmail.com>
2025-06-26 11:55:45 -0700

    Change the cube from moving with Lerp to moving with velocity

--- scripts/Player.cs
+++ scripts/Player.cs
@@ -3,10 +3,14 @@ using Godot;
 public partial class Player : Area3D
 {
 	[Export] private Camera3D camera;
-	[Export] private float speed = 5f;
+	[Export] private float maxSpeed = 10f;
+	[Export] private float acceleration = 10f;
+	
 	
 	private Vector3 target;
 	private Plane movementPlane;
+	
+	private Vector3 velocity = Vector3.Zero;
 	
 	
 	public override void _Ready()
@@ -29,14 +33,31 @@ public partial class Player : Area3D
 
 		var delta = (float)dt;
 		
-		HandleInput();
-		Position = Position.Lerp(target, speed * delta * 2);
+		UpdateTarget();
+		
+		if (Position == target)
+		{
+			return;
+		}
+		
+		velocity += CalculateAcceleration() * delta;
+		velocity = velocity.LimitLength(maxSpeed);
+		Position += velocity * delta;
+		
+		// The cube always slightly overshoots the target,
+		// so once its velocity is going away from the target,
+		// it must have reached and past the target.
+		if (velocity.Dot(Position.DirectionTo(target)) < 0f)
+		{
+			Position = target;
+			velocity = Vector3.Zero;
+		}
 	}
 
 
-	private void HandleInput()
+	private void UpdateTarget()
 	{
-		if (!Input.IsActionPressed("move_unit"))
+		if (!Input.IsActionJustPressed("move_unit"))
 		{
 			return;
 		}
@@ -52,5 +73,37 @@ public partial class Player : Area3D
 		}
 
 		target = worldPos.Value;
+		velocity = Vector3.Zero;
+	}
+
+
+	private Vector3 CalculateAcceleration()
+	{
+		var speedSq = velocity.LengthSquared();
+		var distanceToStop = speedSq / (2f * acceleration);
+		var distanceSqToTarget = Position.DistanceSquaredTo(target);
+
+		var accelerationDirection = 0f;
+		// If the target is close, decelerate.
+		if (distanceSqToTarget <= distanceToStop * distanceToStop)
+		{
+			accelerationDirection = -1f;
+		}
+		// If not close to the target, and not at max speed, accelerate.
+		else if (speedSq < maxSpeed * maxSpeed)
+		{
+			accelerationDirection = 1f;
+		}
+		
+		var directionToTarget = Position.DirectionTo(target);
+		return directionToTarget * acceleration * accelerationDirection;
+	}
+
+
+	// For messing around with, not currently used.
+	private static Vector3 Ease(Vector3 a, Vector3 b, float weight, float curve)
+	{
+		var t = Mathf.Ease(weight, curve);
+		return (1f - t) * a + t * b;
 	}
 }
